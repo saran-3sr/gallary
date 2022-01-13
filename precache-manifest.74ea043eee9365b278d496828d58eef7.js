92a48c491e43f153de4246ff300ede53fd3b6a2c
SaranPradeepJ <saranpradeep2003@gmail.com>
2022-01-13 12:50:35 +0530

    Updates

---
+++ precache-manifest.74ea043eee9365b278d496828d58eef7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b873802f4099b3a1a14d4256d78ced4",
+    "url": "/gallary/index.html"
+  },
+  {
+    "revision": "e0a72a6f51472cd531d0",
+    "url": "/gallary/static/css/main.41a80360.chunk.css"
+  },
+  {
+    "revision": "889af35b4e89395b76f5",
+    "url": "/gallary/static/js/2.ec16f0a6.chunk.js"
+  },
+  {
+    "revision": "e0a72a6f51472cd531d0",
+    "url": "/gallary/static/js/main.282ecee4.chunk.js"
+  },
+  {
+    "revision": "5d4d5cfda307dfd57ec0",
+    "url": "/gallary/static/js/runtime~main.cd2fc00c.js"
+  }
+]);
